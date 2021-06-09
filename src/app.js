85d8c5f76948edefd3b2bad59ef8fe22052ac9ec
Cristian <cristian.gutie@gmail.com>
2021-06-09 18:33:07 -0400

    exercise completed

--- src/app.js
+++ src/app.js
@@ -4,8 +4,69 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import "./assets/img/joker.png";
+import { each, get } from "jquery";
+
+const suitSigns = ["♥", "♠", "♦", "♣"];
+const cardNumber = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K",
+  "JOKER"
+];
+
+let randomCard = { cardSign: "", cardNumber: "" };
+
+function randomCardGenerator() {
+  randomCard.cardSign = suitSigns[Math.floor(Math.random() * suitSigns.length)];
+  randomCard.cardNumber =
+    cardNumber[Math.floor(Math.random() * cardNumber.length)];
+}
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  randomCardGenerator();
+  console.log(randomCard);
+  let randomNumber = document.querySelectorAll(".number");
+  if (randomCard.cardNumber === "JOKER") {
+    document.getElementsByClassName(
+      "number"
+    )[0].innerHTML = `<span class="black vertical-text">${randomCard.cardNumber}</span>`;
+    document.getElementsByClassName(
+      "number"
+    )[1].innerHTML = `<span class="black vertical-text rotate">${randomCard.cardNumber}</span>`;
+  } else {
+    randomNumber.forEach(element => {
+      randomCard.cardSign === "♥" || randomCard.cardSign === "♦"
+        ? (element.innerHTML = `<span class="red">${randomCard.cardNumber}</span>`)
+        : (element.innerHTML = `<span class="black">${randomCard.cardNumber}</span>`);
+    });
+  }
+
+  let randomSign = document.querySelectorAll(".sign");
+  if (randomCard.cardNumber === "JOKER") {
+    document.getElementsByClassName("middle-sign")[0].innerHTML = `<img
+    class="card-img-top"
+    src="./joker.png"
+    alt="playing card sign"
+  />`;
+    document.getElementsByClassName("sign")[0].innerHTML = "";
+    document.getElementsByClassName("sign")[2].innerHTML = "";
+  } else {
+    randomSign.forEach(element => {
+      randomCard.cardSign === "♥" || randomCard.cardSign === "♦"
+        ? (element.innerHTML = `<span class="red">${randomCard.cardSign}</span>`)
+        : (element.innerHTML = `<span class="black">${randomCard.cardSign}</span>`);
+    });
+  }
 };
